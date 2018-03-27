d31ab834bf8ab35c7b9dc23f2d20e409fc98c135
bartwiharto <bartwiharto@gmail.com>
2018-03-26 20:29:30 -0600

    2nd commit - created some data structures and accessed some data

---
+++ answers.js
@@ -0,0 +1,25 @@
+//1. Booleans
+		// 
+//2. Strings
+		//"person@email.com"
+//3. Arrays
+		//let spaceship = ["hull", "laser", "tractor beam", "warp drive"];
+//4. Arrays
+		//["Amy", "Anna", "Andy", "Albert"];
+//5. Objects
+		// [name: "Amy", location: "Class 2", name: "Anna", location: "Class 2", name: "Andy", location: "Class 2"];
+//6. Objects
+		// School = [
+		// 		{name: "Amy", location: "Class 2", favoriteTv: "Pokemon"}
+		// 		{name: "Anna", location: "Class 2", favoriteTv: "Gravity Falls"}
+		// 		{name: "Andy", location: "Class 2", favoriteTv: "Bob's Burger"}
+		// 		{name: "Albert", location: "Class 2", favoriteTv: "Godzilla"}
+		// 	];
+
+//Take it easy
+
+//1. Rainbow = ["Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet"];
+//2. Rainbow[4];
+//3. Bart = [favoriteFood: "Pasta", hobby: "Dance", Location: "Littleton", datatype: "String"];
+//4. Bart[2];
+
